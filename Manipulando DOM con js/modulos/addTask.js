6149f08275e5bfd6e091b418dacc9919517f6ce2
nicoleonard <nikolasleonard@gmail.com>
2023-02-11 16:41:07 -0300

    Añadidas funciones de control de formulario y uso del metodo sort para ordenar las listas

--- Manipulando DOM con js/modulos/addTask.js
+++ Manipulando DOM con js/modulos/addTask.js
@@ -1,5 +1,6 @@
 import checkComplete from "./checkComplete.js";
 import deleteIcon from "./deleteIcon.js";
+import { readTasks } from "./readTasks.js";
 
 export const addTask = (evento) => {
     evento.preventDefault();
@@ -11,6 +12,10 @@ export const addTask = (evento) => {
     const date = calendar.value;
     const dateFormat = moment(date).format('DD/MM/YYYY');
 
+    if(value == "" || date == ""){
+        return;
+    }
+
     input.value=''; //backticks ´´
     calendar.value='';
 
@@ -19,12 +24,14 @@ export const addTask = (evento) => {
         dateFormat
     };
 
+    list.innerHTML = "";
+
     const taskList = JSON.parse(localStorage.getItem("tasks")) || []; 
     taskList.push({value, dateFormat});
     localStorage.setItem("tasks", JSON.stringify(taskList));
 
-    const task = createTask(taskObj);
-    list.appendChild(task);
+    readTasks();
+
   }
   
  
--- Manipulando DOM con js/modulos/readTasks.js
+++ Manipulando DOM con js/modulos/readTasks.js
@@ -1,11 +1,26 @@
 import { createTask } from "./addTask.js";
+import { uniqueDates } from "../services/date.js";
+import dateElement from "./dateElement.js";
 
 export const readTasks = () => {
 const list = document.querySelector('[data-list]');
 
 
     const tasksList = JSON.parse(localStorage.getItem("tasks"));
-    tasksList.forEach((task) =>{
-        list.appendChild(createTask(task));
+    const dates = uniqueDates(tasksList);
+
+    dates.forEach(date => {
+        const dateMoment = moment(date, 'DD/MM/YYYY');
+        list.appendChild(dateElement(date));
+
+        tasksList.forEach((task) =>{
+            const taskDate = moment(task.dateFormat, 'DD/MM/YYYY');
+            const dif = dateMoment.diff(taskDate);
+
+            if(dif == 0){
+                list.appendChild(createTask(task));
+            }
+        });
     });
+
 }
