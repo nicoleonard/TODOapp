08a7c71294aae277bc9f4455a3498f62b9a306aa
nicoleonard <nikolasleonard@gmail.com>
2023-02-11 15:41:56 -0300

    Implementado ejemplo de persistencia de datos en el navegador. Organizacion del codigo.

--- Manipulando DOM con js/script.js
+++ Manipulando DOM con js/script.js
@@ -1,42 +1,11 @@
-import checkComplete from "./modulos/checkComplete.js";
-import deleteIcon from "./modulos/deleteIcon.js";
+import { addTask } from "./modulos/addTask.js";
+import { readTasks } from "./modulos/readTasks.js";
 
 // ejemplo de immediatly invoked function expression IIFE (()=>{})()
 ( () => {
 const btn = document.querySelector('[data-form-btn]');
 
-const addTask = (evento) => {
-  const list = document.querySelector('[data-list]');
-  const task = createTask(evento);
-  list.appendChild(task);
-}
-
-const createTask = (event) => {
-  event.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const calendar = document.querySelector('[data-form-date]');
-  const value = input.value;
-  const date = calendar.value;
-  const dateFormat = moment(date).format('DD/MM/YYYY');
-
-  input.value=''; //backticks ´´
-
-  const task = document.createElement('li');
-  task.classList.add('card');
-  const taskContent = document.createElement('div');
-  taskContent.appendChild(checkComplete());
-  const titleTask = document.createElement('span');
-  titleTask.classList.add("task");
-  titleTask.innerText = value;
-  taskContent.appendChild(titleTask);
-  const dateElement = document.createElement("span");
-  dateElement.innerHTML = dateFormat;
-  task.appendChild(taskContent);
-  task.appendChild(dateElement);
-  task.appendChild(deleteIcon());
-
-};
-
 btn.addEventListener('click', addTask);
+readTasks();
 }
 )();
